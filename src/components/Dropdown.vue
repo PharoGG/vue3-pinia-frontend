831da6816b2505446f4f0b1b568a42d652e2af3f
PharoGG <timick6666@gmail.com>
2024-02-15 16:30:28 +0300

    started

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,14 @@
+<template>
+    <select v-model="selectedEntity">
+      <option value="null">Не выбрано</option>
+      <option value="deal">Сделка</option>
+      <option value="contact">Контакт</option>
+      <option value="company">Компания</option>
+    </select>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const selectedEntity = ref<string|null>(null);
+  </script>
---
+++ src/components/ResultArea.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <div v-for="(result, index) in results" :key="index">
+        {{ result }}
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const results = ref<string[]>([]);
+  </script>
